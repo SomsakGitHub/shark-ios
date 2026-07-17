446b75dac420421f5d0dccb20b91ca8bc62a6b78
somsak <somsak.kaeworasan@outlook.com>
2026-07-18 00:08:39 +0700

    init project.

---
+++ SharkTests/SharkTests.swift
@@ -0,0 +1,19 @@
+//
+//  SharkTests.swift
+//  SharkTests
+//
+//  Created by tiscomacnb2486 on 17/7/2569 BE.
+//
+
+import Testing
+@testable import Shark
+
+struct SharkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
